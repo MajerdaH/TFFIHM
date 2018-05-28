a1268969c1af91ac873f6b94a765eb213bd185ff
HEJER MAJERDA <majerdahejer@gmail.com>
2018-05-28 10:30:38 +0200

    notifications added in jms invoke

--- src/app/pages/expose-soap/expose-soap.component.ts
+++ src/app/pages/expose-soap/expose-soap.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { API_ENDPOINT } from '../../app.constants';
+import { ToastData, ToastOptions, ToastyService } from 'ng2-toasty';
 
 @Component({
   selector: 'app-expose-soap',
   templateUrl: './expose-soap.component.html',
-  styleUrls: ['./expose-soap.component.css']
+  styleUrls: ['./expose-soap.component.css',
+  '../../../../node_modules/ng2-toasty/style-bootstrap.css',
+  '../../../../node_modules/ng2-toasty/style-default.css',
+  '../../../../node_modules/ng2-toasty/style-material.css',
+], encapsulation: ViewEncapsulation.None
 })
 export class ExposeSoapComponent implements OnInit {
+  position = 'bottom-right';
+  title: string;
+  msg: string;
+  showClose = true;
+  timeout = 5000;
+  theme = 'bootstrap';
+  type = 'default';
+  closeOther = false;
 
-  public sub: any;
+public dataFinal: any;
+public sub: any;
 public OperationsArray :Array<any>=[];
 public pname: any;
 public ptype :any;
@@ -20,7 +34,7 @@ public dataOperations : any;
 public dataWsdl : any;
 elmt = {};
   constructor(private route: ActivatedRoute,
-    private router: Router, private http: HttpClient) { }
+    private router: Router, private http: HttpClient, private toastyService: ToastyService) { }
 
   ngOnInit() {
     this.sub = this.route
@@ -40,7 +54,34 @@ elmt = {};
 
     });
   }
-
+  addToast(options) {
+    if (options.closeOther) {
+      this.toastyService.clearAll();
+    }
+    this.position = options.position ? options.position : this.position;
+    const toastOptions: ToastOptions = {
+      title: options.title,
+      msg: options.msg,
+      showClose: options.showClose,
+      timeout: options.timeout,
+      theme: options.theme,
+      onAdd: (toast: ToastData) => {
+        /* added */
+      },
+      onRemove: (toast: ToastData) => {
+        /* removed */
+      }
+    };
+  
+    switch (options.type) {
+      case 'default': this.toastyService.default(toastOptions); break;
+      case 'info': this.toastyService.info(toastOptions); break;
+      case 'success': this.toastyService.success(toastOptions); break;
+      case 'wait': this.toastyService.wait(toastOptions); break;
+      case 'error': this.toastyService.error(toastOptions); break;
+      case 'warning': this.toastyService.warning(toastOptions); break;
+    }
+  }
 
 GO(OperationName: any){
   // tslint:disable-next-line:max-line-length
@@ -48,6 +89,13 @@ GO(OperationName: any){
     console.log(data);
     this.dataWsdl=data;
     console.log(this.dataWsdl);
+     if (this.dataWsdl.root.Status == 'SUCCESS') {
+      // tslint:disable-next-line:max-line-length
+      this.addToast ({title: 'SUCCESS', msg: 'EXPOSITION SOAP CREATED', timeout: 7000, theme: 'bootstrap', position: 'top-right', type: 'success'});
+    } else {
+      this.addToast ({title: 'FAIL', msg: 'An error occured while creating SOAP Exposition', timeout: 7000, theme: 'bootstrap', position: 'top-right', type: 'error'});
+    }
+
   });
   console.log(JSON.stringify({"Operations": {"input":OperationName}}))
 }
